f38b4f8399b53ade6f4369a53f37a8f61faad56c
Virginie <vb@ikimo9.com>
2022-10-18 09:44:22 +0200

    ajout du controller de ressources TodoController

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,3 +24,4 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/a-propos', 'Aproposcontroller@index')->name('apropos');
 
+Route::resource('todos', 'TodoController');
